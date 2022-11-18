253f0f8fb337300d1662107ba603b7952888befb
Hendri <hendriantonius27@gmail.com>
2022-11-18 21:04:51 +0700

    change settings

--- main.py
+++ main.py
@@ -24,5 +24,6 @@ def index():
     return jsonify({"output": text})
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == '__main__':
+    # app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
